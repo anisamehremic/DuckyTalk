5ddc5327af54773b1f927325e2318ddbf5e42eaf
ilmaka <ilmaka@maestralsolutions.com>
2022-04-29 16:20:38 +0200

    initial UI commit

---
+++ DuckyTalk.UI/src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: '', loadChildren: () => import('./public/landing.module').then(m => m.LandingModule) }];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
