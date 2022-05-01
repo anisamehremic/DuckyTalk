a4a779f89fcc796b4981b68253123f31e52929b4
Anisa Mehremic <anisa.mehremic@outlook.com>
2022-05-01 16:11:04 +0200

    docker, seed intial data

---
+++ DuckyTalk/DuckyTalk/Database/DuckyTalkContextData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+#nullable disable
+
+namespace DuckyTalk.Database
+{
+    public partial class DuckyTalkContext
+    {
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.User()
+            {
+                UserId = 1,
+                FirstName = "Anisa",
+                LastName = "Mehremic",
+                Username = "anisam",
+                BirthDate =new DateTime(1998, 11, 4, 14, 28, 58, 693, DateTimeKind.Local),
+                Email = "anisa.mehremic@edu.fit.ba",
+                PasswordHash = "+WQk9mVupe+VOTeMI1a8PsyMmR0=",//anisa123
+                PasswordSalt = "cPYUsauMRpahKHypOM3BIA==",
+            });
+        }
+    }
+}
--- DuckyTalk/DuckyTalk/Program.cs
+++ DuckyTalk/DuckyTalk/Program.cs
@@ -1,5 +1,8 @@
+using DuckyTalk.Database;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,12 @@ namespace DuckyTalk
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope()) {
+               var service = scope.ServiceProvider.GetRequiredService<DuckyTalkContext>();
+                service.Database.Migrate();
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
