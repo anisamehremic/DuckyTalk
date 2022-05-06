70822b7a77dc1fc3fed775b87c6aba0af49c1259
Anisa Mehremic <anisa.mehremic@outlook.com>
2022-05-06 18:30:46 +0200

    mc

--- DuckyTalk/DuckyTalk.Model/Models/User.cs
+++ DuckyTalk/DuckyTalk.Model/Models/User.cs
@@ -12,9 +12,5 @@ namespace DuckyTalk.Model
         public string Username { get; set; }
         public DateTime BirthDate { get; set; }
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string PasswordSalt { get; set; }
-        public DateTime? LastLoginAt { get; set; }
-        public bool? IsDeleted { get; set; }
     }
 }
