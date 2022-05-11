0b722be108c96e5ede695e700e4bd019ba4d6d72
Anisa Mehremic <anisa.mehremic@outlook.com>
2022-05-11 18:55:25 +0200

    loading data from json files

--- DuckyTalk/DuckyTalk/Database/DuckyTalkContextData.cs
+++ DuckyTalk/DuckyTalk/Database/DuckyTalkContextData.cs
@@ -19,6 +19,8 @@ namespace DuckyTalk.Database
                 PasswordHash = "+WQk9mVupe+VOTeMI1a8PsyMmR0=",//anisa123
                 PasswordSalt = "cPYUsauMRpahKHypOM3BIA==",
             });
+            modelBuilder.Entity<Messages>().HasData(Helpers.LoadingDataHelper.LoadJsonFromFile<Messages>("MessagesJson.json"));
         }
+        
     }
 }
---
+++ DuckyTalk/DuckyTalk/Helpers/LoadingDataHelper.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DuckyTalk.Helpers
+{
+    public class LoadingDataHelper
+    {
+        public static string GetFilePathJsonData(string fileName)
+        {
+            string exeFile = AppDomain.CurrentDomain.BaseDirectory;
+            string exeDir = Path.GetDirectoryName(exeFile);
+            string fullPath = exeDir.ToString() + "/DataSeedJson/" + fileName;
+            return fullPath;
+        }
+        public static List<T> LoadJsonFromFile<T>(string fileName)
+        {
+            using (StreamReader r = new StreamReader(GetFilePathJsonData(fileName)))
+            {
+                string json = r.ReadToEnd();
+                List<T> items = JsonConvert.DeserializeObject<List<T>>(json);
+                return items;
+            }
+        }
+    }
+}
