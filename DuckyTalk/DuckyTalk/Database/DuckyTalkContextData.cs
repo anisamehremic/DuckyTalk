2b2ee9e717333d3f8aa978a713b5c493699c7407
Anisa Mehremic <anisa.mehremic@outlook.com>
2022-05-12 08:05:52 +0200

    mc

--- DuckyTalk/DuckyTalk/Database/DuckyTalkContextData.cs
+++ DuckyTalk/DuckyTalk/Database/DuckyTalkContextData.cs
@@ -19,7 +19,7 @@ namespace DuckyTalk.Database
                 PasswordHash = "+WQk9mVupe+VOTeMI1a8PsyMmR0=",//anisa123
                 PasswordSalt = "cPYUsauMRpahKHypOM3BIA==",
             });
-            modelBuilder.Entity<Messages>().HasData(Helpers.LoadingDataHelper.LoadJsonFromFile<Messages>("MessagesJson.json"));
+            modelBuilder.Entity<Message>().HasData(Helpers.LoadingDataHelper.LoadJsonFromFile<Message>("MessagesJson.json"));
         }
         
     }
