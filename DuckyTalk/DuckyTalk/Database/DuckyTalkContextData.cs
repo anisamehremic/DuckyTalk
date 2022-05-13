c266aa6bae63cbc309eaf992d7854d137224f444
Lejla Mujakić <lmujakic@outlook.com>
2022-05-13 14:43:49 +0200

    mc

--- DuckyTalk/DuckyTalk/Database/DuckyTalkContextData.cs
+++ DuckyTalk/DuckyTalk/Database/DuckyTalkContextData.cs
@@ -30,91 +30,91 @@ namespace DuckyTalk.Database
                 PasswordHash = "+WQk9mVupe+VOTeMI1a8PsyMmR0=",//anisa123
                 PasswordSalt = "cPYUsauMRpahKHypOM3BIA==",
             });
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.Technology()
+            modelBuilder.Entity<Technology>().HasData(new DuckyTalk.Database.Technology()
             {
                 TechnologyId = 1,
                 Name = "NaN",
                 Description = "",
                 IsDeleted = false,
             });
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.Technology()
+            modelBuilder.Entity<Technology>().HasData(new DuckyTalk.Database.Technology()
             {
                 TechnologyId = 2,
                 Name = "SQL",
                 Description = "SQL is a standard language for accessing and manipulating databases.",
                 IsDeleted = false,
             });   
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.Technology()
+            modelBuilder.Entity<Technology>().HasData(new DuckyTalk.Database.Technology()
             {
                 TechnologyId = 3,
                 Name = "Java",
                 Description = "Java is an object-oriented programming language that produces software for multiple platforms.",
                 IsDeleted = false,
             });
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.Interest()
+            modelBuilder.Entity<Interest>().HasData(new DuckyTalk.Database.Interest()
             {
                 InterestId = 1,
                 Name = "Music",
                 Description = "Rock music",
             });
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.Interest()
+            modelBuilder.Entity<Interest>().HasData(new DuckyTalk.Database.Interest()
             {
                 InterestId = 2,
                 Name = "Movies",
                 Description = "Comedy",
             });  
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.UserTechnology()
+            modelBuilder.Entity<UserTechnology>().HasData(new DuckyTalk.Database.UserTechnology()
             {
                 UserTechnologyId = 1,
                 UserId = 1,
                 TechnologyId = 1,
                 IsDeleted=false,
             });   
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.UserTechnology()
+            modelBuilder.Entity<UserTechnology>().HasData(new DuckyTalk.Database.UserTechnology()
             {
                 UserTechnologyId = 2,
                 UserId = 2,
                 TechnologyId = 1,
                 IsDeleted=false,
             });  
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.UserTechnology()
+            modelBuilder.Entity<UserTechnology>().HasData(new DuckyTalk.Database.UserTechnology()
             {
                 UserTechnologyId = 3,
                 UserId = 1,
                 TechnologyId = 2,
                 IsDeleted=false,
             });   
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.UserInterest()
+            modelBuilder.Entity<UserInterest>().HasData(new DuckyTalk.Database.UserInterest()
             {
                 UserInterestId = 1,
                 UserId = 1,
                 InterestId = 1,
                 IsActive = false,
             });     
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.UserInterest()
+            modelBuilder.Entity<UserInterest>().HasData(new DuckyTalk.Database.UserInterest()
             {
                 UserInterestId = 2,
                 UserId = 2,
                 InterestId = 1,
                 IsActive = true,
             });     
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.UserInterest()
+            modelBuilder.Entity<UserInterest>().HasData(new DuckyTalk.Database.UserInterest()
             {
                 UserInterestId = 3,
                 UserId = 1,
                 InterestId = 2,
                 IsActive = true,
             });    
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.UserMessage()
+            modelBuilder.Entity<UserMessage>().HasData(new DuckyTalk.Database.UserMessage()
             {
                 UserMessageId = 1,
                 UserId = 1,
                 MessageId = 1,
                 TimeShowed = new DateTime(2022, 5, 12, 14, 28, 58, 693, DateTimeKind.Local),
             });    
-            modelBuilder.Entity<User>().HasData(new DuckyTalk.Database.UserMessage()
+            modelBuilder.Entity<UserMessage>().HasData(new DuckyTalk.Database.UserMessage()
             {
-                UserMessageId = 1,
+                UserMessageId = 2,
                 UserId = 2,
                 MessageId = 2,
                 TimeShowed = new DateTime(2022, 5, 12, 15, 28, 58, 693, DateTimeKind.Local),
